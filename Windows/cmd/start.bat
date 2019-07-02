65bb4f3252124d63f5866a72c87517cebc265b1d
whzecomjm <whzecomjm@foxmail.com>
2019-07-03 00:19:01 +0300

    update confs

--- Windows/cmd/start.bat
+++ Windows/cmd/start.bat
@@ -3,7 +3,7 @@
 :: https://blog.csdn.net/xufeng0991/article/details/41309603 
 @echo off
 start "MouseInc" "D:\Shortcuts\MouseInc.lnk"
-:: start "ProcessLasso" "D:\Shortcuts\ProcessLasso.lnk"
+start "ProcessLasso" "D:\Shortcuts\ProcessLasso.lnk"
 start "Mem Reduct" "D:\Shortcuts\memreduct.lnk"
 start "whz.ahk" "E:\Program Docs\AutoHotKey\whz.ahk"
 start "TIM" "D:\Shortcuts\TIM.lnk" 
