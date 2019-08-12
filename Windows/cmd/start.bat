2286bc9eda27d476697161d056e72f0e2d0d6cb9
whzecomjm <whzecomjm@foxmail.com>
2019-08-12 23:09:49 +0800

    renew start address

--- Windows/cmd/start.bat
+++ Windows/cmd/start.bat
@@ -3,7 +3,7 @@
 :: https://blog.csdn.net/xufeng0991/article/details/41309603 
 @echo off
 start "MouseInc" "D:\Shortcuts\MouseInc.lnk"
-start "whz.ahk" "D:\Program Files\Dism++\whz.ahk"
+start "whz.ahk" "E:\我的坚果云\whz.ahk"
 start "ProcessLasso" "D:\Program Files\Dism++\ProcessLasso64\ProcessLasso.exe"
 start "MemReduct" "D:\Program Files\Dism++\memreduct\memreduct.exe"
 start "TrafficMonitor" "D:\Program Files\Dism++\TrafficMonitor\TrafficMonitor.exe"
